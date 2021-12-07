407d85bfa112b0acf7f4279eadf493bafc5db5d5
LoveDH <dhyeok1996@gmail.com>
2021-12-07 17:40:43 +0900

    UPDATE: tpot prediction with saving version

--- 택배_운송량_예측/tpot_checkpoint/pipeline_gen_1_idx_0_2021.12.07_15-56-58.py
+++ 택배_운송량_예측/tpot_checkpoint/pipeline_gen_1_idx_0_2021.12.07_15-56-58.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -30.039478019886893
-exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=4, min_child_weight=10, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.55, verbosity=0)
+# Average CV score on the training set was: -29.194453074268644
+exported_pipeline = RandomForestRegressor(bootstrap=False, max_features=0.05, min_samples_leaf=7, min_samples_split=13, n_estimators=100)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
--- 택배_운송량_예측/tpot_checkpoint/pipeline_gen_2_idx_0_2021.12.07_16-15-29.py
+++ 택배_운송량_예측/tpot_checkpoint/pipeline_gen_2_idx_0_2021.12.07_16-15-29.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import AdaBoostRegressor
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -29.788615705674015
-exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=4, min_child_weight=6, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.4, verbosity=0)
+# Average CV score on the training set was: -28.933637223071656
+exported_pipeline = AdaBoostRegressor(learning_rate=0.01, loss="linear", n_estimators=100)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
